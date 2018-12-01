168ca2af3a0dabd4bf523165f74d5e4a5af07205
Snezunka <tanyasivnitska@gmail.com>
2018-12-01 20:33:13 +0200

    Test 2 - add method getClassNumber with test

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -2,12 +2,18 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest {
+    private MainClass mainClass = new MainClass();
 
     @Test
     public void testGetLocalNumber() {
-        MainClass mainClass = new MainClass();
+
         int expectedResult =14;
         int actualResult = mainClass.getLocalNumber();
         Assert.assertEquals("Actual result " + actualResult + " is NOT equal expected " + expectedResult, actualResult, expectedResult);
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        Assert.assertTrue("Class number is less than 45",  mainClass.getClassNumber() > 45);
     }
 }
